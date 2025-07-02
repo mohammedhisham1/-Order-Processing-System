d29119354b12a0da3b80dea02cea99db86521d7c
mohammedhisham1 <mohamad92moh@gmail.com>
2025-07-02 23:43:41 +0300

    Refactor run.py to set application port from environment variable and enable external access

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import logging
 from app import create_app
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,4 +14,5 @@ logging.basicConfig(
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
